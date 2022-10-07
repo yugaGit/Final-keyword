831744cca7fe1f7edd093f310c1b1f022fab4348
Yugaraj.B <114376104+yugaGit@users.noreply.github.com>
2022-10-07 15:04:58 +0530

    Add files via upload
     final keyword used before the data type and  return type. once the value assigned unable to  change .

---
+++ words.java
@@ -0,0 +1,13 @@
+public class words{
+ void namechange(  int phno){
+phno= 123412;
+System.out.println(phno);
+}
+public static void main(String[]args){
+ words emp=new words();
+  final int phno=2121;
+ emp.namechange(phno);
+}
+}
+  
+
